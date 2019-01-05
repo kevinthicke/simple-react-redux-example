c7a4f3801926e177a095ddfc9c662208f3560401
Kevin Cabrera <keviincabrera@gmail.com>
2019-01-05 14:43:10 +0000

    Refac

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { increaseAction, decreaseAction } from './actions';
 
-// React component
 class Counter extends Component {
     render() {
       const { value, onIncreaseClick, onDecreaseClick } = this.props
       return (
         <div>
-          <span>{value}</span>
-          <button onClick={onIncreaseClick}>Increase</button>
           <button onClick={onDecreaseClick}>Decrease</button> 
+          <button onClick={onIncreaseClick}>Increase</button>
+          <span>{value}</span>
         </div>
       )
     }
   }
 
-function mapStateToProps(state) {
-    return {
-        value: state.count
-    }
-}
+const mapStateToProps = state => ({ value: state.count });
 
-function mapDispatchToProps(dispatch) {
-    return {
-        onIncreaseClick: () => dispatch(increaseAction),
-        onDecreaseClick: () => dispatch(decreaseAction)
-    }
-}
+const mapDispatchToProps = dispatch => ({
+    onIncreaseClick: () => dispatch(increaseAction),
+    onDecreaseClick: () => dispatch(decreaseAction)
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
